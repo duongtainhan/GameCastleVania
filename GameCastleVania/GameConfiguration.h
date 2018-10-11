dcd74755cd6b17bd48f5a23930cae5e0c38115c7
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-12 01:22:37 +0700

    The concept of Const

--- GameCastleVania/GameConfiguration.h
+++ GameCastleVania/GameConfiguration.h
@@ -1,9 +1,17 @@
-#pragma once
+﻿#pragma once
 #include<map>
 using namespace std;
 
 #define GLOBALS_D(name) GameConfiguration::GetConfiguration(name)
 #define GLOBALS_S(name) GameConfiguration::GetConfigurationString(name)
+
+/*
+Các khái niệm về con trỏ:
+- const : đặt địa chỉ của biến là vùng nhớ hằng, giá trị bên trong vùng nhớ đó sẽ không bị thay đổi
+- const: giúp đảm bảo an toàn dữ liệu cho biến
+- Khi biến của const được khởi tạo giá trị, thì sẽ không được gán được giá trị khác, mọi tác động đến vùng nhớ
+đều bị ngăn chặn
+*/
 
 class GameConfiguration
 {
