ea87ade00d4e4a317a0e4ae73023305407c0748b
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-15 22:56:10 +0700

    Game.h : Init, Update, Render game

---
+++ GameCastleVania/Game.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+class Game
+{
+	static Game* instance;
+public:
+	Game();
+	~Game();
+
+	static Game* getInstance();
+
+	//Các hàm khởi tạo, cập nhật và vẽ game
+	void GameInit();
+	void GameUpdate();
+	void GameRender();
+};
