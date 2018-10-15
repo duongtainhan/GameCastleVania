90e0aa79d7b05699a7d91aa47c5f99ccc0c35d9c
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-15 23:45:36 +0700

    GameTime.h

---
+++ GameCastleVania/GameTime.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <Windows.h>
+
+class GameTime
+{
+	//Thời gian bắt đầu
+	DWORD startTime;
+	//Khoảng thời gian delay
+	DWORD tickPerFrame;
+	//Khoảng thời gian kể từ lúc bắt đầu đến hiện tại
+	DWORD deltaTime;
+public:
+	GameTime(void);
+	~GameTime(void);
+	DWORD getStartTime();
+	void setStartTime(DWORD startTime);
+	DWORD getTickPerFrame();
+	void setTickPerFrame(DWORD tickPerFrame);
+	void init(DWORD tickPerFrame);
+	DWORD getDeltaTime();
+	void setDeltaTime(DWORD deltaTime);
+	GameTime(DWORD tickPerFrame);
+	//Đến thời điểm hiện tại
+	virtual bool atTime();
+};
