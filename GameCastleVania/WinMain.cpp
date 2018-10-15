9af411d14552e95f434f29a0b4fed2a962bbeb86
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-15 22:44:40 +0700

    Set rect for Render image (Texture)

--- GameCastleVania/WinMain.cpp
+++ GameCastleVania/WinMain.cpp
@@ -8,30 +8,36 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+
 using namespace std;
 
-
+#include "GameTexture.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	WindowGame::getInstance()->initHandleWindows(hInstance, nCmdShow);
 	srand(time(NULL));
 
-	/* GetTickCount: lấy thời gian hiện tại của hệ thống */
-	/* Tính thời gian bắt đầu */
+	// GetTickCount: lấy thời gian hiện tại của hệ thống
+	// Tính thời gian bắt đầu
 	DWORD startTime = GetTickCount();
 
 	MSG msg;
 	ZeroMemory(&msg, sizeof(msg));
 
-	/* Tính thời gian cho mỗi frame */
+	// Tính thời gian cho mỗi frame
 	DWORD timePerFrame = 1000 / GLOBALS_D("fps");
 
-	/* vòng lặp game */
+	//Khởi tạo hình
+	GameTexture gameTexture;
+	gameTexture.Init("file/viet_nam.png");
+
+	//Vòng lặp game
 	while (msg.message != WM_QUIT)
 	{
-		/* now là thời gian hiện tại */
+		// now là thời gian hiện tại
 		DWORD now = GetTickCount();
+
 		/* nếu khoảng thời gian kể từ lúc bắt đầu tới bây giờ lớn hơn thời gian cho mỗi frame thì khởi tạo lại thời gian bắt đầu
 		là bây giờ*/
 		DWORD deltaTime = now - startTime;
@@ -44,11 +50,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 				DispatchMessage(&msg);
 			}
 			float time = deltaTime / 1000.0f;
-			GameDirectX::getInstance()->BeginGraphics();//bat dau ve len backbuffer
-			GameDirectX::getInstance()->EndGraphics();// ket thuc ve len backbuffer
-			GameDirectX::getInstance()->PresentBackBuffer();// ve backbuffer len man hinh
-		}
 
+			//Bắt đầu vẽ lên backbuffer
+			GameDirectX::getInstance()->BeginGraphics();
+
+			RECT viewRect;
+			SetRect(&viewRect, 66, 30, 182, 147);
+			//Vẽ hình
+			gameTexture.Render(9, 9,&viewRect);
+			//Kết thúc vẽ lên backbuffer
+			GameDirectX::getInstance()->EndGraphics();
+			//Vẽ backbuffer lên màn hình
+			GameDirectX::getInstance()->PresentBackBuffer();
+		}
 	}
 	return nCmdShow;
 }
