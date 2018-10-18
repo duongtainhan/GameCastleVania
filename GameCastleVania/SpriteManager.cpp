1cbf34f0f605b3a1e24a56218cd1a964a1b643fb
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-19 01:04:47 +0700

    SpriteManager: Used to save all sprites in the game

---
+++ GameCastleVania/SpriteManager.cpp
@@ -0,0 +1,27 @@
+﻿#include "SpriteManager.h"
+SpriteManager * SpriteManager::instance = 0;
+SpriteManager * SpriteManager::getInstance()
+{
+	if (instance == 0)
+	{
+		instance = new SpriteManager();
+	}
+	return instance;
+}
+Sprite * SpriteManager::getSprite(int spriteInfo)
+{
+	return getInstance()->sprites[spriteInfo];
+}
+SpriteManager::SpriteManager()
+{
+	// Thêm SPRITE_COUNT phần tử cho list sprite
+	for (size_t i = 0; i < SPRITE_COUNT; i++)
+	{
+		sprites._Add(new Sprite());
+	}
+
+	// Thêm sprite ở đây
+}
+SpriteManager::~SpriteManager()
+{
+}
---
+++ GameCastleVania/SpriteManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include"List.h"
+#include"Sprite.h"
+
+#define SPR SpriteManager::getSprite
+
+// Tạo 1 sprite info để lưu thông tin của các sprite
+enum SPRITE_INFO
+{
+	// Đối tượng không có hình ảnh ta sẽ đặt số âm
+	SPRITE_INFO_GROUND = -1,
+	// Đối tượng có hình ảnh ta sẽ đặt số dương
+	SPRITE_INFO_SIMON = 0,
+
+	SPRITE_COUNT
+};
+
+class SpriteManager
+{
+	// Lưu tất cả sprite vào 1 list
+	List<Sprite*> sprites;
+
+	static SpriteManager* instance;
+public:
+	static SpriteManager* getInstance();
+
+	// dùng để lấy sprite từ spriteManager
+	static Sprite* getSprite(int spriteInfo);
+
+	SpriteManager();
+	~SpriteManager();
+};
