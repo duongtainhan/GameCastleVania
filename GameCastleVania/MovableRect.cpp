c51c73bae5affedc4f5ce45d602de81eee583e8c
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-18 23:06:38 +0700

    Configure rect in tile map

---
+++ GameCastleVania/MovableRect.cpp
@@ -0,0 +1,38 @@
+#include "MovableRect.h"
+
+MovableRect::MovableRect()
+{
+	dx = 0;
+	dy = 0;
+}
+
+void MovableRect::setDx(float dx)
+{
+	this->dx = dx;
+}
+void MovableRect::setDy(float dy)
+{
+	this->dy = dy;
+}
+float MovableRect::getDx()
+{
+	return dx;
+}
+float MovableRect::getDy()
+{
+	return dy;
+}
+
+void MovableRect::goX()
+{
+	Rect::moveX(dx);
+}
+
+void MovableRect::goY()
+{
+	Rect::moveY(dy);
+}
+
+MovableRect::~MovableRect()
+{
+}
---
+++ GameCastleVania/MovableRect.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Rect.h"
+
+// Đây là lớp Rect có thể di chuyển
+class MovableRect :
+	public Rect
+{
+	// Độ dời di chuyển của đối tượng
+	float dx, dy;
+public:
+	virtual void setDx(float dx);
+	virtual void setDy(float dy);
+	float getDx();
+	float getDy();
+	// Phương thức di chuyển
+	void goX();
+	void goY();
+	MovableRect();
+	~MovableRect();
+};
