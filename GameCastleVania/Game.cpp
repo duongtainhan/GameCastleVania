65d1f7c87ed3c9e644b5b042124099d25e04814d
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-18 23:31:50 +0700

    tilemap in GameInit

--- GameCastleVania/Game.cpp
+++ GameCastleVania/Game.cpp
@@ -16,6 +16,18 @@ Game::~Game()
 }
 void Game::GameInit()
 {
+	// Khởi tạo tilemap
+	tilemap = new Tilemap();
+	tilemap->Init("file/demotile");
+	Camera::getInstance()->set(
+		0,
+		400,
+		// Kích thước của camera bằng với kích thước của backbuffer
+		GLOBALS_D("backbuffer_width"),
+		GLOBALS_D("backbuffer_height"));
+
+	// Di chuyển camera theo phương phải 1 px
+	Camera::getInstance()->setDx(1);
 }
 
 void Game::GameUpdate()
