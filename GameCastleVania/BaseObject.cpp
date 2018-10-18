b171b56c9e81c9411df2a902e8eecebf5c3a2a1d
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-19 00:55:56 +0700

    Object settings include: init, update and render object + Animation + Sprite of this object

---
+++ GameCastleVania/BaseObject.cpp
@@ -0,0 +1,94 @@
+﻿#include "BaseObject.h"
+
+void BaseObject::setSprite(Sprite * sprite)
+{
+	this->sprite = sprite;
+}
+
+Sprite * BaseObject::getSprite()
+{
+	return this->sprite;
+}
+
+bool BaseObject::getPauseAnimation()
+{
+	return pauseAnimation;
+}
+
+void BaseObject::setPauseAnimation(bool pauseAnimation)
+{
+	this->pauseAnimation = pauseAnimation;
+}
+
+bool BaseObject::getIsLastFrameAnimationDone()
+{
+	return isLastFrameAnimationDone;
+}
+
+void BaseObject::setIsLastFrameAnimationDone(bool isLastFrameAnimationDone)
+{
+	this->isLastFrameAnimationDone = isLastFrameAnimationDone;
+}
+
+void BaseObject::onInitFromFile(ifstream& fs, int mapHeight)
+{
+	int collisionType, x, y, width, height;
+	// Đọc collision type x y width height từ file
+	fs >> collisionType >> x >> y >> width >> height;
+	// y của đối tượng trong file là tọa độ hướng từ trên xuống
+	// y của game chúng ta làm thì gốc tọa độ bên dưới nên ta chuyển nó về cho đúng tọa độ logic
+	y = mapHeight - y;
+	// Khởi tạo x y width height cho đối tượng
+	set(x, y, width, height);
+}
+
+void BaseObject::update(float dt)
+{
+	// Di chuyển đối tượng trước khi cập nhật
+	goX();
+	goY();
+
+	setIsLastFrameAnimationDone(false);
+
+	// nếu đối tượng này có hình và không bị pauseAnimation thì ta cập nhật animation cho nó
+	if (!pauseAnimation && getSprite() != NULL)
+	{
+		if (animationGameTime.atTime())
+		{
+			// Cập nhật animation cho đối tượng
+			sprite->update(animationIndex, frameIndex);
+			if (frameIndex == 0)
+			{
+				// Nếu frame cuối đã chạy thì sau khi cập nhật frameIndex sẽ là 0
+				setIsLastFrameAnimationDone(true);
+			}
+		}
+	}
+
+	onUpdate(dt);
+}
+
+void BaseObject::onUpdate(float dt)
+{
+	// Mặc định sẽ cho chạy animation
+	setPauseAnimation(false);
+}
+
+void BaseObject::render(Camera* camera)
+{
+	float xView, yView;
+	// Tính tọa độ view để vẽ đối tượng lên màn hình
+	camera->convertWorldToView(getX(), getY(), xView, yView);
+	// Vẽ đối tượng lên màn hình
+	sprite->render(xView, yView, animationIndex, frameIndex);
+}
+
+BaseObject::BaseObject()
+{
+	setSprite(NULL);
+	animationGameTime.init(GLOBALS_D("object_animation_time_default"));
+}
+
+BaseObject::~BaseObject()
+{
+}
---
+++ GameCastleVania/BaseObject.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "MovableRect.h"
+#include"Sprite.h"
+#include"GameTime.h"
+#include"Camera.h"
+
+// Đây là đối tượng của game. Kế thừa lại lớp đối tượng có thể chuyển động
+class BaseObject :
+	public MovableRect
+{
+	Sprite* sprite;
+	// Animation hiện tại và frame index hiện tại
+	int animationIndex, frameIndex;
+
+	// Dùng để làm chậm animation
+	GameTime animationGameTime;
+
+	// Dừng animation
+	bool pauseAnimation;
+	// Kiểm tra xem frame cuối cùng đã hoàn thành hay chưa
+	bool isLastFrameAnimationDone;
+public:
+	void setSprite(Sprite*sprite);
+	Sprite* getSprite();
+
+	bool getPauseAnimation();
+	virtual void setPauseAnimation(bool pauseAnimation);
+
+	bool getIsLastFrameAnimationDone();
+	virtual void setIsLastFrameAnimationDone(bool isLastFrameAnimationDone);
+
+	// Khởi tạo đối tượng đọc từ file
+	virtual void onInitFromFile(ifstream& fs, int mapHeight);
+
+	// Update đối tượng
+	virtual void onUpdate(float dt);
+	// Cập nhật của đối tượng sau mỗi lần vẽ lại
+	void update(float dt);
+	// Vẽ đối tượng
+	virtual void render(Camera* camera);
+
+	BaseObject();
+	~BaseObject();
+};
