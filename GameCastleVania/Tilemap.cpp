4146fcad84ac33c72b3753d2031e112a19f545b0
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-18 22:53:31 +0700

    Configure the tile map

---
+++ GameCastleVania/Tilemap.cpp
@@ -0,0 +1,49 @@
+﻿#include "Tilemap.h"
+#include<fstream>
+using namespace std;
+
+Tilemap::Tilemap()
+{
+}
+Tilemap::~Tilemap()
+{
+}
+void Tilemap::Init(const char * tilesheetPath, const char * matrixPath)
+{
+	// Khởi tạo tilesheet
+	tilesheet = new GameTexture();
+	tilesheet->Init(tilesheetPath);
+
+	// Khởi tạo matrix
+	fstream fs(matrixPath);
+
+	// Đọc các giá trị
+	fs >> tileRows >> tileColumns >> tileWidth >> tileHeight >> tilesheetColumns;
+
+	// Khởi tạo ma trận
+	matrix = new int*[tileRows];
+
+	for (size_t rowIndex = 0; rowIndex < tileRows; rowIndex++)
+	{
+		matrix[rowIndex] = new int[tileColumns];
+		for (size_t columnIndex = 0; columnIndex < tileColumns; columnIndex++)
+		{
+			// Đọc thông số ma trận tile file
+			fs >> matrix[rowIndex][columnIndex];
+		}
+	}
+
+}
+
+void Tilemap::Init(const char * folderPath)
+{
+	// Đường dẫn tilesheet và matrix
+	string folderPathString = (string)folderPath;
+	string tilesheetString = folderPathString;
+	tilesheetString.append("/tilesheet.png");
+	string matrixPathString = folderPathString;
+	matrixPathString.append("/matrix.dat");
+
+	// Khởi tạo bằng phương thức trên
+	Init(tilesheetString.c_str(), matrixPathString.c_str());
+}
---
+++ GameCastleVania/Tilemap.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include"GameTexture.h"
+class Tilemap
+{
+	// Chiều rộng tile
+	int tileWidth;
+	// Chiều dài tile
+	int tileHeight;
+	// Số lượng dòng trong matrix
+	int tileRows;
+	// Số lượng cột trong matrix
+	int tileColumns;
+	// Số lượng cột trong tilesheet
+	int tilesheetColumns;
+
+	// Tilesheet
+	GameTexture* tilesheet;
+
+	// Ma trận tile
+	int** matrix;
+public:
+	// tilesheetPath: đường dẫn tilesheet
+	// matrixPath : đường dẫn file ma trận
+	
+	void Init(const char* tilesheetPath, const char* matrixPath);
+	// folderPath : đường dẫn thư mục chứa ma trận và tilesheet
+	void Init(const char* folderPath);
+	Tilemap();
+	~Tilemap();
+};
