b3d01a874ec502897fd16ae6322ce5be2e5c7fe0
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-18 23:14:17 +0700

    --- Camera ---

---
+++ GameCastleVania/Camera.cpp
@@ -0,0 +1,39 @@
+﻿#include "Camera.h"
+#include<d3dx9.h>
+
+Camera * Camera::instance = 0;
+Camera * Camera::getInstance()
+{
+	if (instance == 0)
+	{
+		instance = new Camera();
+	}
+	return instance;
+}
+
+void Camera::convertWorldToView(float xWorld, float yWorld, float & xView, float & yView)
+{
+	// Ma trận biến đổi world to view
+	D3DXMATRIX matrixWorldToView;
+	D3DXMatrixIdentity(&matrixWorldToView);
+	matrixWorldToView._22 = -1;
+	matrixWorldToView._41 = -this->getX();
+	matrixWorldToView._42 = this->getY();
+
+	// Nhân ma trận đó với xWorld yWorld ta được xView yView
+	D3DXVECTOR3 pos3(xWorld, yWorld, 1);
+	D3DXVECTOR4 MatrixResult;
+	D3DXVec3Transform(&MatrixResult, &pos3, &matrixWorldToView);
+
+	xView = MatrixResult.x;
+	yView = MatrixResult.y;
+}
+
+Camera::Camera()
+{
+}
+
+
+Camera::~Camera()
+{
+}
---
+++ GameCastleVania/Camera.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include"MovableRect.h"
+
+// Camera là 1 hình chữ nhật có thể di chuyển nên sẽ kế thừa lại lớp MovableRect
+class Camera : public MovableRect
+{
+	static Camera* instance;
+public:
+	static Camera* getInstance();
+
+	/* phương thức chuyển đổi world qua view
+	Tham số đầu vào là xWorld yWorld
+	Tham số cần lấy là xView yView (nên có dấu &, cần xem lại khái niệm về tham chiếu C++ để hiểu)
+	Phương thức này trong sách game có giở sách ra xem sẽ thấy
+	*/
+	void convertWorldToView(float xWorld, float yWorld, float& xView, float& yView);
+	Camera();
+	~Camera();
+};
