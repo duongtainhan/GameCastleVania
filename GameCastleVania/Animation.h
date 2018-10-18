8599ff38f8735302f4abb36b86afd229cdf5f9ed
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-18 21:00:59 +0700

    create class Animation

---
+++ GameCastleVania/Animation.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include"List.h"
+#include"FrameAnimation.h"
+
+class Animation
+{
+public:
+	Animation();
+	~Animation();
+
+	/* Danh sách frame */
+	List<FrameAnimation*> frames;
+	/* Chuyển sang frame kế khi truyền vào current frame */
+	int NextFrame(int currentFrame);
+};
