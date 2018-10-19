ec029603067996949735b09ca738b9d55f451e70
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-19 19:32:19 +0700

    set up physics for the object

---
+++ GameCastleVania/PhysicsOject.cpp
@@ -0,0 +1,79 @@
+﻿#include "PhysicsOject.h"
+
+PhysicsObject::PhysicsObject()
+{
+	setAx(0);
+	// set trọng lực mặc định của đối tượng lấy từ GameConfiguration.cpp
+	setAy(GLOBALS_D("object_default_ay"));
+	setVx(0);
+	setVy(0);
+	// bật physics cho đối tượng
+	setPhysicsEnable(true);
+}
+
+void PhysicsObject::onUpdate(float dt)
+{
+	if (getPhysicsEnable())
+	{
+		// v = v0 + at
+		setVx(getVx() + getAx()*dt);
+		setVy(getVy() + getAy()*dt);
+		// x = x0 + vx*t = x0 + dx => dx = vx*t
+		setDx(getVx()*dt);
+		setDy(getVy()*dt);
+	}
+}
+
+PhysicsObject::~PhysicsObject()
+{
+}
+
+float PhysicsObject::getAx()
+{
+	return ax;
+}
+
+float PhysicsObject::getAy()
+{
+	return ay;
+}
+
+float PhysicsObject::getVx()
+{
+	return vx;
+}
+
+float PhysicsObject::getVy()
+{
+	return vy;
+}
+
+void PhysicsObject::setAx(float ax)
+{
+	this->ax = ax;
+}
+
+void PhysicsObject::setAy(float ay)
+{
+	this->ay = ay;
+}
+
+void PhysicsObject::setVx(float vx)
+{
+	this->vx = vx;
+}
+
+void PhysicsObject::setVy(float vy)
+{
+	this->vy = vy;
+}
+
+bool PhysicsObject::getPhysicsEnable()
+{
+	return physicsEnable;
+}
+
+void PhysicsObject::setPhysicsEnable(bool physicsEnable)
+{
+	this->physicsEnable = physicsEnable;
+}
---
+++ GameCastleVania/PhysicsOject.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "BaseObject.h"
+
+class PhysicsObject :
+	public BaseObject
+{
+	// Gia tốc và vận tốc
+	float ax, ay, vx, vy;
+	// Cho phép trọng lực hay không
+	bool physicsEnable;
+public:
+	float getAx();
+	float getAy();
+	float getVx();
+	float getVy();
+	virtual void setAx(float ax);
+	virtual void setAy(float ay);
+	virtual void setVx(float vx);
+	virtual void setVy(float vy);
+
+	bool getPhysicsEnable();
+	virtual void setPhysicsEnable(bool physicsEnable);
+
+	// override lại phương thức update của BaseObject
+	void onUpdate(float dt) override;
+
+	PhysicsObject();
+	~PhysicsObject();
+};
