cf3ce1c2470d73d214d6d9097863d88e4e0cfdb3
duongtainhan <40598538+duongtainhan@users.noreply.github.com>
2018-10-25 19:17:27 +0700

    ---- Zombie : Collision ---

--- GameCastleVania/Zombie.cpp
+++ GameCastleVania/Zombie.cpp
@@ -1,4 +1,11 @@
-#include "Zombie.h"
+﻿#include "Zombie.h"
+
+void Zombie::onCollision(MovableRect * other, float collisionTime, int nx, int ny)
+{
+	// ngăn chặn di chuyển
+	preventMovementWhenCollision(collisionTime, nx, ny);
+	PhysicsObject::onCollision(other, collisionTime, nx, ny);
+}
 
 Zombie::Zombie()
 {
--- GameCastleVania/Zombie.h
+++ GameCastleVania/Zombie.h
@@ -5,6 +5,7 @@ class Zombie :
 	public PhysicsObject
 {
 public:
+	void onCollision(MovableRect* other, float collisionTime, int nx, int ny) override;
 	Zombie();
 	~Zombie();
 };
